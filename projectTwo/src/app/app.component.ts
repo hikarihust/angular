d37a0a712a496384f5b33d9b06b1db5dc2c49301
vudinhquang <vudinhquangk53@gmail.com>
2020-01-26 17:22:36 +0900

    Template Setting - Init Project

---
+++ projectTwo/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // template: '<h1>My Application</h1>',
+  templateUrl: '../template/app.component.html',
+  // styles: [`.an2Cls { 
+  //   background-color: red;
+  // }
+  // .tsCls {
+  //     background-color: yellow;
+  // }`],
+  styleUrls: ['../template/app.component.css']
+})
+export class AppComponent {
+  title = 'projectOne';
+}
