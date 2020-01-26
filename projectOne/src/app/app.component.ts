fb69a773cacef2cbf5da91b50921b22c15227e4c
vudinhquang <vudinhquangk53@gmail.com>
2020-01-26 16:43:14 +0900

    template, TemplateURL

--- projectOne/src/app/app.component.ts
+++ projectOne/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>My Application</h1>',
-  // templateUrl: './app.component.html',
+  // template: '<h1>My Application</h1>',
+  templateUrl: '../template/app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
