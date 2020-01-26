abe9eabefdbdb091ea104b3740425c8cbecda6e8
vudinhquang <vudinhquangk53@gmail.com>
2020-01-26 16:09:44 +0900

    Component Metadata - Selector

--- projectOne/src/app/app.component.ts
+++ projectOne/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: '<h1>My Application</h1>',
   // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
