6cd6a316d70560aac880e14d4e0ae36ebdc5535a
vudinhquang <vudinhquangk53@gmail.com>
2020-02-05 00:56:08 +0900

    Sử dụng Pipe cơ bản - Lowercase, Uppercase

--- projectFour/src/app/app.module.ts
+++ projectFour/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+
+import { PipeComponent } from './pipe/pipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipeComponent
   ],
   imports: [
     BrowserModule,
