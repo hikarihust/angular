94a43e09e91b47a053fb002d65323eef135d3712
vudinhquang <vudinhquangk53@gmail.com>
2020-02-05 00:46:51 +0900

    List Course with Pipe & Custom Pipe - Init

---
+++ projectFour/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['../template/app.component.css']
+})
+export class AppComponent {
+  title = 'project 4 - List Course with Pipe & Custom Pipe';
+}
