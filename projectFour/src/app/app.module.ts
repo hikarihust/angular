15015f3dcf79b916fd214e95f001bde1038c897d
vudinhquang <vudinhquangk53@gmail.com>
2020-02-08 19:50:03 +0900

    CourseCategoryPipe

--- projectFour/src/app/app.module.ts
+++ projectFour/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -20,6 +21,7 @@ import { ControlComponent } from './project-list/components/control.component';
 import { CourseComponent } from './project-list/components/course.componet';
 import { CourseCapitalizePipe } from './project-list/pipes/course-capitalize.pipe';
 import { CourseSummaryPipe } from './project-list/pipes/course-summary.pipe';
+import { CourseCategoryPipe } from './project-list/pipes/course-category.pipe';
 
 @NgModule({
   declarations: [
@@ -40,7 +42,8 @@ import { CourseSummaryPipe } from './project-list/pipes/course-summary.pipe';
     ControlComponent,
     CourseComponent,
     CourseCapitalizePipe,
-    CourseSummaryPipe
+    CourseSummaryPipe,
+    CourseCategoryPipe
   ],
   imports: [
     BrowserModule,
