566c4f533c4bf236da2e5f1500530dd280ea176f
vudinhquang <vudinhquangk53@gmail.com>
2020-04-14 00:45:17 +0900

    Structural Directive

--- projectSix/src/app/app.module.ts
+++ projectSix/src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { NgModule,  }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AppComponent }   from './app.component';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+// StructuralComponent
+import { StructuralComponent } from './structural/structural.component';
 
 @NgModule({
+  imports:      [ BrowserModule],
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+    AppComponent,
+    StructuralComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projectSix/src/app/structural/structural.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'zvn-structural',
+    templateUrl: './structural.component.html'
+})
+
+export class StructuralComponent {
+	students: string[] = ["john", "marry", "peter"];
+
+}
