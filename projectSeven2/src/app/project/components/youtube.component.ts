fafcab5c101285b0fb3a2e94f6e7113fc6c82262
vudinhquang <vudinhquangk53@gmail.com>
2020-05-01 00:27:58 +0900

    CookieService 01

--- projectSeven2/src/app/project/components/youtube.component.ts
+++ projectSeven2/src/app/project/components/youtube.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { VideoService } from './../services/video.service';
 import { Video } from './../class/video.class';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
 	selector: 'zvn-project-youtube',
   templateUrl: './../templates/youtube.component.html',
-  providers: [ VideoService ]
+  providers: [ VideoService, CookieService ]
 })
 export class YoutubeComponent implements OnInit {
 
   currentVideo: Video;
 
 	constructor(
-    private _videoService: VideoService
+    private _videoService: VideoService,
+    private _cookieService: CookieService
   ) {
 
   }
 
   ngOnInit() {
     this.currentVideo = this._videoService.getCurrentVideo();
+    console.log(this._cookieService);
   }
 
   onChangeVideo(event: Video) {
