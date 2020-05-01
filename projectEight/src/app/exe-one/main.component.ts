b84bdf046694064719c2624c23484b0871d1fab5
vudinhquang <vudinhquangk53@gmail.com>
2020-05-02 01:01:07 +0900

    RouterLinkActive

--- projectEight/src/app/exe-one/main.component.ts
+++ projectEight/src/app/exe-one/main.component.ts
@@ -11,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
       <router-outlet></router-outlet>
     </div>
     <div class="panel-footer">
-      <a class="link" routerLink="/">Home</a>
-      <a class="link" routerLink="/about">About</a>
-      <a class="link" [routerLink]="['/contact']">Contact</a>
+      <a class="link"
+        routerLink="/"
+        routerLinkActive="active"
+        [routerLinkActiveOptions]="{ exact: true }"
+        >Home</a>
+      <a class="link" routerLink="/about" routerLinkActive="active">About</a>
+      <a class="link" [routerLink]="['/contact']" routerLinkActive="active">Contact</a>
     </div>
   </div>
 	`
