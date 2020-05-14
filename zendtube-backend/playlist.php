6e2baca7dbad036bc2e9cf1fea2315345e25e56f
vudinhquang <vudinhquangk53@gmail.com>
2020-05-14 16:13:04 +0900

    Lấy thông tin playlist youtube - Get playlist 01

--- zendtube-backend/playlist.php
+++ zendtube-backend/playlist.php
@@ -18,6 +18,66 @@
     <![endif]-->
 </head>
 <body>
+<?php
+        require_once('libs/define.php'); 
+        require_once('libs/my-func.php');
+        
+        $playlistInfo  = [];
+        $playlistID    = isset($_POST['playlist-id']) ? trim($_POST['playlist-id']) : '';
+        $result     = '';
+        $msg        = '';
+
+        // Step 01 - Get Playlist Info
+        $strInfoURL = createURL([
+            'part'  => 'snippet',
+            'id'    => $playlistID,
+            'key'   => $API_KEY
+        ]);
+
+        $dataInfoReturn   = json_decode(file_get_contents($API_URL . 'playlists?' . $strInfoURL), true);
+
+        if($dataInfoReturn['items']){
+            $snippet = $dataInfoReturn['items'][0]['snippet'];
+            $playlistInfo['id']            = $playlistInfo;
+            $playlistInfo['publishedAt']   = $snippet['publishedAt'];
+            $playlistInfo['title']         = $snippet['title'];
+            $playlistInfo['slug']          = createSlug($snippet['title']);
+            $playlistInfo['description']   = $snippet['description'];
+            $playlistInfo['thumbnails']    = $snippet['thumbnails']['standard']['url'];
+        }
+
+        echo "<pre>";
+        print_r($playlistInfo);
+        echo "</pre>";
+
+        if (isset($_POST["btnPreview"])){
+            // $result = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $videoInfo['id'] . '" frameborder="0" allow="accelerometer; 
+            //     autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        } else if (isset($_POST["btnSave"])){
+            // $videoIDs = file_get_contents($FILE_VIDEO_TXT);
+
+            // if (strpos($videoIDs,  "##" . $videoID . "##") === false){ 
+            //     // Step 1: Lưu $videoID vào file videos.txt
+            //     file_put_contents($FILE_VIDEO_TXT, $videoIDs . "##" . $videoID . "##");   
+
+            //     // Step 2: Lưu thông tin video $videoInfo của $videoID vào file videos.json    
+            //     $json   = json_decode(file_get_contents($FILE_VIDEO_JSON), true);
+            //     $json[] = $videoInfo;
+
+            //     file_put_contents($FILE_VIDEO_JSON, json_encode($json));
+
+            //     $msg   = 'Video đã được lưu thành công';
+            // }else {
+            //     // Xuất thông báo video này đã lưu rồi
+            //     $msg   = 'Video này đã được lưu rồi';
+            // }
+        } else if (isset($_POST["btnClear"])){
+            $result     = '';
+            $playlistInfo  = [];
+            $playlistID    = '';
+        }
+    ?>
+
     <div class="container-fluid">
 
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -30,7 +90,7 @@
                     <form action="playlist.php" method="POST" class="form-inline" role="form">
                         <div class="form-group">
                             <label class="sr-only" for="">label</label>
-                            <input type="text" class="form-control" id="" name="playlist-id" placeholder="Video ID" value="">
+                            <input type="text" class="form-control" id="" name="playlist-id" placeholder="Video ID" value="<?php echo $playlistID; ?>">
                         </div>
                         <button type="submit" name="btnPreview" class="btn btn-primary">Preview Video</button>
                         <button type="submit" name="btnSave" class="btn btn-success">Save To File</button>
@@ -46,6 +106,9 @@
                     <h3 class="panel-title">Result</h3>
                 </div>
                 <div class="panel-body">
+                    <?php 
+                        if($msg) echo $msg ;
+                    ?>
                 </div>
                 <div class="panel-footer">
                 </div>
