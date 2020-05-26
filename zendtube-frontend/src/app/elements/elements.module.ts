c41a8bc1fb58a659eb4860487acf6addc350914c
vudinhquang <vudinhquangk53@gmail.com>
2020-05-27 01:21:45 +0900

    Xây dựng Module Elements - Widget Ads 01

---
+++ zendtube-frontend/src/app/elements/elements.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { WidgetAdsComponent } from './widget-ads/widget-ads.component';
+
+
+
+@NgModule({
+  declarations: [
+    WidgetAdsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    WidgetAdsComponent
+  ]
+})
+export class ElementsModule { }
---
+++ zendtube-frontend/src/app/elements/widget-ads/widget-ads.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'zvn-widget-ads',
+  templateUrl: './widget-ads.component.html',
+})
+export class WidgetAdsComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+    console.log("WidgetAdsComponent");
+  }
+}
