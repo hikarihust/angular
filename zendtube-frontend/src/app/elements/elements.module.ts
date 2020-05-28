6710d279d2ca9b761281bc72775d229a130d2707
vudinhquang <vudinhquangk53@gmail.com>
2020-05-29 00:12:41 +0900

    Widget Social

--- zendtube-frontend/src/app/elements/elements.module.ts
+++ zendtube-frontend/src/app/elements/elements.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { WidgetAdsComponent } from './widget-ads/widget-ads.component';
 import { CommonModule } from '@angular/common';
 import { WidgetBannerComponent } from './widget-banner/widget-banner.component';
+import { WidgetSocialComponent } from './widget-social/widget-social.component';
 
 
 
 @NgModule({
   declarations: [
     WidgetAdsComponent,
-    WidgetBannerComponent
+    WidgetBannerComponent,
+    WidgetSocialComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     WidgetAdsComponent,
-    WidgetBannerComponent
+    WidgetBannerComponent,
+    WidgetSocialComponent
   ]
 })
 export class ElementsModule { }
---
+++ zendtube-frontend/src/app/elements/widget-social/widget-social.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[zvn-widget-social]',
+  templateUrl: './widget-social.component.html',
+})
+export class WidgetSocialComponent implements OnInit {
+  ngOnInit() {
+  }
+}
