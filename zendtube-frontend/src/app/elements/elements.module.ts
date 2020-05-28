6dacd46e194228ab3802985d6d95b1cf988df28b
vudinhquang <vudinhquangk53@gmail.com>
2020-05-29 01:38:27 +0900

    Element Header

--- zendtube-frontend/src/app/elements/elements.module.ts
+++ zendtube-frontend/src/app/elements/elements.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { WidgetAdsComponent } from './widget-ads/widget-ads.component';
 import { CommonModule } from '@angular/common';
+
+// Widget
+import { WidgetAdsComponent } from './widget-ads/widget-ads.component';
 import { WidgetBannerComponent } from './widget-banner/widget-banner.component';
 import { WidgetSocialComponent } from './widget-social/widget-social.component';
 import { WidgetMenuTopComponent } from './widget-menu-top/widget-menu-top.component';
 import { WidgetMenuMainComponent } from './widget-menu-main/widget-menu-main.component';
 
-
+// Element
+import { ElmHeaderComponent } from './elm-header/elm-header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { WidgetMenuMainComponent } from './widget-menu-main/widget-menu-main.com
     WidgetBannerComponent,
     WidgetSocialComponent,
     WidgetMenuTopComponent,
-    WidgetMenuMainComponent
+    WidgetMenuMainComponent,
+    ElmHeaderComponent
   ],
   imports: [
     CommonModule
@@ -24,7 +28,8 @@ import { WidgetMenuMainComponent } from './widget-menu-main/widget-menu-main.com
     WidgetBannerComponent,
     WidgetSocialComponent,
     WidgetMenuTopComponent,
-    WidgetMenuMainComponent
+    WidgetMenuMainComponent,
+    ElmHeaderComponent
   ]
 })
 export class ElementsModule { }
---
+++ zendtube-frontend/src/app/elements/elm-header/elm-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: '[zvn-elm-header]',
+  templateUrl: './elm-header.component.html',
+})
+export class ElmHeaderComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
