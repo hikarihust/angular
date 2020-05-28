69c476d443a8b357a61b618641fdf36202d99603
vudinhquang <vudinhquangk53@gmail.com>
2020-05-28 23:51:26 +0900

    Widget Banner

--- zendtube-frontend/src/app/elements/elements.module.ts
+++ zendtube-frontend/src/app/elements/elements.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { WidgetAdsComponent } from './widget-ads/widget-ads.component';
 import { CommonModule } from '@angular/common';
+import { WidgetBannerComponent } from './widget-banner/widget-banner.component';
 
 
 
 @NgModule({
   declarations: [
-    WidgetAdsComponent
+    WidgetAdsComponent,
+    WidgetBannerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    WidgetAdsComponent
+    WidgetAdsComponent,
+    WidgetBannerComponent
   ]
 })
 export class ElementsModule { }
---
+++ zendtube-frontend/src/app/elements/widget-banner/widget-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[zvn-widget-banner]',
+  templateUrl: './widget-banner.component.html',
+})
+export class WidgetBannerComponent implements OnInit {
+  ngOnInit() {
+  }
+}
