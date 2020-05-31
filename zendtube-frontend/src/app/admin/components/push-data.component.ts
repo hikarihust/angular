3fe3374cb6042a4dc27521e199a2cb725db58be0
vudinhquang <vudinhquangk53@gmail.com>
2020-05-31 19:43:02 +0900

    Playlist 01

--- zendtube-frontend/src/app/admin/components/push-data.component.ts
+++ zendtube-frontend/src/app/admin/components/push-data.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+
 import { dbAds } from './../../../environments/db-ads';
+import { dbPlaylist } from './../../../environments/db-playlist';
 
 @Component({
   selector: 'zvn-admin-push-data',
@@ -16,10 +18,20 @@ export class PushDataComponent implements OnInit {
   }
 
   onPushData() {
+    // Ads
+    /*
     const adsRef = this._db.list("ads");
 
     dbAds.ads.forEach((ads: any) => {
       adsRef.push(ads);
+    });
+    */
+
+    // Playlist
+    const PlaylistRef = this._db.list("playlist");
+
+    dbPlaylist.playlist.forEach((playlist: any) => {
+      console.log(playlist);
     });
   }
 }
