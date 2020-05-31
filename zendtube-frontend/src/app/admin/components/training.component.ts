7ed12221356821403e1ac1a3e709b1ea94fdd1ef
vudinhquang <vudinhquangk53@gmail.com>
2020-05-31 14:42:51 +0900

    Firebase API 02

--- zendtube-frontend/src/app/admin/components/training.component.ts
+++ zendtube-frontend/src/app/admin/components/training.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -10,9 +10,24 @@ import { Observable } from 'rxjs';
 export class TrainingComponent implements OnInit {
   items: Observable<any[]>;
   item: Observable<any>;
+  itemRef: AngularFireObject<any>;
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items').valueChanges();
     this.item = db.object('/items/sefrftgt').valueChanges();
+
+    // Remove data
+    // const promise = db.object('/items/abc').remove();
+    // promise
+    //   .then(_ => console.log('success'))
+    //   .catch(err => console.log(err, 'You dont have access!'));
+
+    // Saving data
+    // const itemRef = db.object('/items/abc');
+    // itemRef.set({ id: 3, ordering: 200, name: 'typescript'});
+
+    // Updating data
+    // const itemRef = db.object('/items/abc');
+    // itemRef.update({ ordering: 150 });
   }
   ngOnInit() {
   }
