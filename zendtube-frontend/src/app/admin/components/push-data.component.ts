3e930d9b2f0da56c9f49fa0e094c0e2e9e972384
vudinhquang <vudinhquangk53@gmail.com>
2020-05-31 19:08:54 +0900

    Ads 02

--- zendtube-frontend/src/app/admin/components/push-data.component.ts
+++ zendtube-frontend/src/app/admin/components/push-data.component.ts
@@ -2,14 +2,24 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { dbAds } from './../../../environments/db-ads';
 
 @Component({
   selector: 'zvn-admin-push-data',
   templateUrl: './../templates/push-data.component.html',
 })
 export class PushDataComponent implements OnInit {
-  constructor(db: AngularFireDatabase) {
+  constructor(
+    private _db: AngularFireDatabase) {
   }
   ngOnInit() {
+  }
+
+  onPushData() {
+    const adsRef = this._db.list("ads");
+
+    dbAds.ads.forEach((ads: any) => {
+      adsRef.push(ads);
+    });
   }
 }
