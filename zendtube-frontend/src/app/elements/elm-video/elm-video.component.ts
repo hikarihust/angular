47121f37f4bd968113bb7e30bbf860679188ca48
vudinhquang <vudinhquangk53@gmail.com>
2020-06-07 22:10:42 +0900

    Widget Featured 02

--- zendtube-frontend/src/app/elements/elm-video/elm-video.component.ts
+++ zendtube-frontend/src/app/elements/elm-video/elm-video.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Video } from 'src/app/shared/defines/video.class';
+
 @Component({
   selector: '[zvn-elm-video]',
   templateUrl: './elm-video.component.html',
 })
 export class ElmVideoComponent implements OnInit {
   @Input("show-desc") showDesc: boolean = false;
+  @Input("zvn-elm-video") item: Video = null;
   constructor() {
   }
   ngOnInit() {
